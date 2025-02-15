d22e87be81db81ea728ae88657b2a3ac214c9502
John Wang <47154554+johnwang16@users.noreply.github.com>
2025-02-15 15:22:55 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,6 @@ declare host
 declare password
 declare port
 declare username
-declare secondary_host
-declare secondary_password
-declare secondary_port
-declare secondary_username
-
 
     host=$(bashio::services "mqtt" "host")
     password=$(bashio::services "mqtt" "password")
@@ -16,7 +11,7 @@ declare secondary_username
     username=$(bashio::services "mqtt" "username")
 
 
-#cat /data/options.json
+cat /data/options.json
 cp -f /data/options.json /mochad-mqtt.json
 sed -i -n '/{/{N/"name":/D}p' /mochad-mqtt.json
 sed -i '/"name":/{s/,/:{/}' /mochad-mqtt.json
@@ -36,13 +31,9 @@ sed -i "s/\"host\": \"xxxxxxxx\"/\"host\": \"$host\"/" /mochad-mqtt.json
 sed -i "s/\"user\": \"xxxxxxxx\"/\"user\": \"$username\"/" /mochad-mqtt.json
 sed -i "s/\"password\": \"xxxxxxxx\"/\"password\": \"$password\"/" /mochad-mqtt.json
 sed -i "s/\"port\": \"xxxxxxxx\"/\"port\": \"$port\"/" /mochad-mqtt.json
-sed -i "s/\"secondary_host\": \"xxxxxxxx\"/\"secondary_host\": \"$secondary_host\"/" /mochad-mqtt.json
-sed -i "s/\"secondary_user\": \"xxxxxxxx\"/\"secondary_user\": \"$secondary_username\"/" /mochad-mqtt.json
-sed -i "s/\"secondary_password\": \"xxxxxxxx\"/\"secondary_password\": \"$secondary_password\"/" /mochad-mqtt.json
-sed -i "s/\"secondary_port\": \"xxxxxxxx\"/\"secondary_port\": \"$secondary_port\"/" /mochad-mqtt.json
 
 
-#cat /mochad-mqtt.json
+cat /mochad-mqtt.json
 
 perl /mochad-mqtt.pl
 
